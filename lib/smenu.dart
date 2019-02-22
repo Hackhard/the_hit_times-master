fcbe202dee557bf12d3b26f5e9c61e5db9adcc73
Asher Smith <sumank343@gmail.com>
2019-02-23 01:14:41 +0530

    Added Images to the menu

--- lib/smenu.dart
+++ lib/smenu.dart
@@ -1,59 +1,71 @@
 import 'package:flutter/material.dart';
 
-import 'package:the_hit_times/card_ui.dart';
+//import 'package:the_hit_times/card_ui.dart';
 
 class SMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /*return Container(
-      height: double.infinity,
-      width: double.infinity,
-      color: Colors.blue,
-    );*/
     return ListView(
       children: <Widget>[
         fourGrid(),
         Container(
           margin: EdgeInsets.only(left:10.0,right:10.0,top: 8.0,bottom: 8.0),
-          height: 210.0,
+          height: 300.0,
           width: double.infinity,
-          color: Colors.blue,
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              margin: EdgeInsets.only(left: 10.0,bottom: 10.0),
-              child: Text(
-                "About The HIT Times",
-                style:TextStyle(color: Colors.white,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.w700,
+
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: ExactAssetImage('assets/images/about_us.jpeg'),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                      Colors.black26,
+                      BlendMode.darken,
+                    ),
+                  ),
                 ),
               ),
-            ),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: bottomTitle(
+                      caption : 'About Us'
+                  ),
+              ),
+            ],
           ),
-        )
+        ),
       ],
     );
   }
 }
 
 
-final bottomTitle = new Container(
-  height: 40.0,
-  width: double.infinity,
-  margin: EdgeInsets.all(0.0),
-  //color: Colors.black12,
-  child:Container(
-    margin: EdgeInsets.only(left: 10.0),
-    child: Text('Campus Adda',
-      style:TextStyle(color: Colors.white,
-        fontSize: 22.0,
-        fontWeight: FontWeight.w800,
+class bottomTitle extends StatelessWidget {
+  bottomTitle({this.caption});
+  final String caption;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40.0,
+      width: double.infinity,
+      margin: EdgeInsets.all(0.0),
+      //color: Colors.black12,
+      child:Container(
+        margin: EdgeInsets.only(left: 10.0),
+        child: Text( caption,
+          style:TextStyle(color: Colors.white,
+            fontSize: 22.0,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
       ),
-    ),
-  ),
-);
+    );
+  }
+}
 
 class fourGrid extends StatelessWidget{
   @override
@@ -72,20 +84,57 @@ class fourGrid extends StatelessWidget{
               margin: EdgeInsets.only(top: 5.0,right: 2.0,bottom: 5.0,left: 0.0),
               height: MediaQuery.of(context).size.width * 0.5,
               width: MediaQuery.of(context).size.width * 0.47,
-              color: Colors.black,
-              child: InkWell(
-                onTap: (){
-                  Navigator.of(context).push(new MaterialPageRoute(
-                    builder: (BuildContext context) => new ReadIssue(),
-                  ));
-                },
+
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/campus_news.jpg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black26,
+                            BlendMode.darken
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment.bottomLeft,
+                      child: bottomTitle(
+                          caption : 'Campus News'
+                      )
+                  ),
+                ],
               ),
             ),
             Container(
               margin: EdgeInsets.only(top: 5.0,right: 0.0,bottom: 5.0,left: 2.0),
               height: MediaQuery.of(context).size.width * 0.5,
               width: MediaQuery.of(context).size.width * 0.47,
-              color: Colors.black,
+
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/read_issue.jpeg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black26,
+                            BlendMode.darken
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment.bottomLeft,
+                      child: bottomTitle(
+                          caption : 'App Exclusive'
+                      )
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -97,13 +146,58 @@ class fourGrid extends StatelessWidget{
               margin: EdgeInsets.only(top: 5.0,right: 2.0,bottom: 5.0,left: 0.0),
               height: MediaQuery.of(context).size.width * 0.5,
               width: MediaQuery.of(context).size.width * 0.47,
-              color: Colors.black,
+
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/weeklies.jpeg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black26,
+                            BlendMode.darken
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment.bottomLeft,
+                      child: bottomTitle(
+                          caption : 'Weeklies'
+                      )
+                  ),
+                ],
+              ),
             ),
+
             Container(
               margin: EdgeInsets.only(top: 5.0,right: 0.0,bottom: 5.0,left: 2.0),
               height: MediaQuery.of(context).size.width * 0.5,
               width: MediaQuery.of(context).size.width * 0.47,
-              color: Colors.black,
+
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/read_issue.jpeg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black26,
+                            BlendMode.darken
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: bottomTitle(
+                      caption : 'Read Issue'
+                    )
+                  ),
+                ],
+              ),
             ),
           ],
         ),
